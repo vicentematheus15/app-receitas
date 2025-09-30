a2faa78ec881332ae7f9d375772a3089ac047181
Matheus Vicete <vicentematheus1507@gmail.com>
2025-09-29 21:15:20 -0300

    Implement navigation state in App.js to conditionally render Home component or 404 message

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 import Home from './telas/Home';
 
 export default function App() {
+  const [navegacao, setNavegacao] = useState('')
+
+  const navegar = (tela) => {
+    setNavegacao(tela)
+  }
+
   return (
     <View style={styles.container}>
-      <Home></Home>
+
+      {(navegacao === 'Home') ? (
+        <Home></Home>
+      ) : (
+        <Text>
+          Página não encontrada 404
+        </Text>
+      )}
     </View>
   );
 }
