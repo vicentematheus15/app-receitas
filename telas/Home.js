3fe7012f7efdaad9123e42dfaf213eb34b83593b
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-30 20:32:07 -0300

    Remove background color from Home component styles

--- telas/Home.js
+++ telas/Home.js
@@ -22,7 +22,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f8f8f8', 
         padding: 20
     },
     title: {
