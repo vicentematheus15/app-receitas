515dd116b99bc7fb98ab30612798d575abfdcba2
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-30 20:50:19 -0300

    Refactor Receitas component to include state management and wrap content in ScrollView for better layout

--- telas/receitas.js
+++ telas/receitas.js
@@ -1,13 +1,22 @@
-import {View, Text, TouchableOpacity} from 'react-native'
+import { useState } from 'react'
+import {View, Text, TouchableOpacity, ScrollView} from 'react-native'
 
 export default function Receitas({navegar}){
+    const [view, setView] = useState('lista')
+    const [receitas, setReceitas] = useState([])
+
+
     return(
         <View>
-            <Text>Receitas</Text>
+            <ScrollView>
 
-            <TouchableOpacity onPress={() => navegar('Home')}>
-                <Text>Voltar</Text>
-            </TouchableOpacity>
+                <Text>Receitas</Text>
+
+                <TouchableOpacity onPress={() => navegar('Home')}>
+                    <Text>Voltar</Text>
+                </TouchableOpacity>
+
+            </ScrollView>
         </View>
     )
 }
