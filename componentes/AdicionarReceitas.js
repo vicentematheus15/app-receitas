4cd0b35c159039079b781d53247c919a07b52355
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-30 21:39:12 -0300

    Add AdicionarReceitas component and integrate it into Receitas for recipe addition

---
+++ componentes/AdicionarReceitas.js
@@ -0,0 +1,10 @@
+import { ScrollView, Text} from 'react-native';
+
+export default function AdicionarReceitas() {
+    
+    return(
+        <ScrollView>
+            <Text>Adicionar Receita</Text>
+        </ScrollView>
+    )
+}
--- telas/receitas.js
+++ telas/receitas.js
@@ -1,43 +1,59 @@
-import { useState } from 'react'
-import {View, Text, TouchableOpacity, ScrollView, Image, StyleSheet} from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+  StyleSheet,
+} from "react-native";
 
-export default function Receitas({navegar}){
-    const [view, setView] = useState('lista')
-    const [receitas, setReceitas] = useState([12])
+import AdicionarReceitas from "../componentes/AdicionarReceitas";
 
+export default function Receitas({ navegar }) {
+  const [view, setView] = useState("lista");
+  const [receitas, setReceitas] = useState([12]);
 
-    return(
+  return (
+    <View>
+      <ScrollView>
         <View>
-            <ScrollView>
-                <View>
-                    <TouchableOpacity onPress={() => navegar('Home')}>
-                        <Image 
-                        style={styles.imagem}
-                        source={require('../assets/arrow-bg.png')}
-                        />
-                    </TouchableOpacity>
-                   <Text>Receitas</Text>
-                </View>
-
-                {(view === 'lista') ? (
-                    <View>
-                        {(receitas.length === 0) ? (
-                            <Text>Nenhuma receita cadastrada</Text>
-                        ) : (
-                            <View></View>
-                        )}
-                    </View>
-                ) : (
-                    <View></View>
-                )}
-            </ScrollView>
+          <TouchableOpacity onPress={() => navegar("Home")}>
+            <Image
+              style={styles.imagem}
+              source={require("../assets/arrow-bg.png")}
+            />
+          </TouchableOpacity>
+          <Text>Receitas</Text>
         </View>
-    )
+
+        {view === "lista" ? (
+          <View>
+            <TouchableOpacity onPress={() => setView("formulario")}>
+                <Text>Adicionar receita</Text>
+            </TouchableOpacity>
+            
+            {receitas.length === 0 ? (
+              <Text>Nenhuma receita cadastrada</Text>
+            ) : (
+              <View>
+
+              </View>
+            )}
+          </View>
+        ) : (
+          <View>
+            <AdicionarReceitas></AdicionarReceitas>
+          </View>
+        )}
+      </ScrollView>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    imagem: {
-        borderWidth: 1,
-        borderColor: '#111',
-        borderRadius: 10
-    }
-})
+  imagem: {
+    borderWidth: 1,
+    borderColor: "#111",
+    borderRadius: 10,
+  },
+});
