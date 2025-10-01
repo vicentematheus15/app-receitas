436c6b5f17485848ae8e5ae9d20de5b5e4afeffb
Matheus Vicente <vicentematheus1507@gmail.com>
2025-09-30 21:24:09 -0300

    Update Receitas component to initialize receitas state and improve layout structure

--- telas/receitas.js
+++ telas/receitas.js
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import {View, Text, TouchableOpacity, ScrollView, Image} from 'react-native'
+import {View, Text, TouchableOpacity, ScrollView, Image, StyleSheet} from 'react-native'
 
 export default function Receitas({navegar}){
     const [view, setView] = useState('lista')
-    const [receitas, setReceitas] = useState([])
+    const [receitas, setReceitas] = useState([12])
 
 
     return(
@@ -11,13 +11,25 @@ export default function Receitas({navegar}){
             <ScrollView>
                 <View>
                     <TouchableOpacity onPress={() => navegar('Home')}>
-                        <Image source={require('../assets/arrow-bg.png')}/>
+                        <Image 
+                      
+                        source={require('../assets/arrow-bg.png')}
+                        />
                     </TouchableOpacity>
                    <Text>Receitas</Text>
                 </View>
 
-
-
+                {(view === 'lista') ? (
+                    <View>
+                        {(receitas.length === 0) ? (
+                            <Text>Nenhuma receita cadastrada</Text>
+                        ) : (
+                            <View></View>
+                        )}
+                    </View>
+                ) : (
+                    <View></View>
+                )}
             </ScrollView>
         </View>
     )
