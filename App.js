ad88189ac9494f0489241b488d6ae1bc3290d8a4
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-07 19:49:30 -0300

    Refactor App component to manage navigation state and improve rendering logic

--- App.js
+++ App.js
@@ -4,23 +4,32 @@ import Home from './telas/Home';
 import Receitas from './telas/Receitas';
 
 export default function App() {
+  // Estado que guarda a tela atual (padrão: 'Home')
   const [navegacao, setNavegacao] = useState('Home')
 
+  // Função para trocar de tela
   const navegar = (tela) => {
-    setNavegacao(tela)
+    setNavegacao(tela) // Altera o estado para a tela recebida como parâmetro
   }
 
+  // Função que escolhe qual componente (tela) será exibido
   const renderizacao = () => {
     switch(navegacao) {
       case 'Home':
+        // Renderiza a tela Home e passa a função navegar como prop
         return <Home navegar={navegar}></Home>
+
       case 'Receitas':
+        // Renderiza a tela Receitas e também passa a função navegar
         return <Receitas navegar={navegar}></Receitas>
+        
       default:
-        <Text>Erro 404</Text>
+         // Caso a tela não seja conhecida, exibe uma mensagem de erro
+        return <Text>Erro 404</Text>
     }
   }
 
+  // Retorno do componente principal
   return (
     <View style={styles.container}>
     {renderizacao()}
