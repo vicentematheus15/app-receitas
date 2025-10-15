b5c3adab70274c3d63eb76680fd0b5df875344be
Matheus Vicente <vicentematheus1507@gmail.com>
2025-10-14 21:45:22 -0300

    Add input fields for recipe name, ingredients, and preparation method in AdicionarReceitas component

--- componentes/AdicionarReceitas.js
+++ componentes/AdicionarReceitas.js
@@ -1,10 +1,40 @@
-import { ScrollView, Text} from 'react-native';
+import { useState, useEffect } from 'react';
+import { ScrollView, Text, TouchableOpacity} from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function AdicionarReceitas() {
+    const [nome, setNome] = useState('')
+    const [ingredientes, setIngredientes] = useState('')
+    const [modoPreparo, setModoPreparo] = useState('')
+
+
+
+        
+
+
     
     return(
         <ScrollView>
             <Text>Adicionar Receita</Text>
+            <TextInput 
+                value={nome} 
+                onChangeText={setNome}
+                placeholder='Digite o nome da receita'
+            />
+            <TextInput
+                value={ingredientes}
+                onChangeText={setIngredientes}
+                placeholder='Digite os ingredientes'
+            />
+            <TextInput
+                value={modoPreparo}
+                onChangeText={setModoPreparo}
+                placeholder='Digite o modo de preparo'
+            />
+
+            <TouchableOpacity onPress={salvar}>
+                <Text>Salvar</Text>
+            </TouchableOpacity>
         </ScrollView>
     )
 }
